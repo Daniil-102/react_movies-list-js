ff6b0072d5ce9d987fcebdf2ffdf45af517bfd63
Daniel Kononchuk <11fm2020arli@gmail.com>
2024-11-19 13:38:01 +0200

    resolve task

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,3 +1,46 @@
 import './MovieCard.scss';
 
-export const MovieCard = () => <>Put the card here</>;
+export const MovieCard = ({ movie }) => (
+  <div className="card" data-cy="Movie">
+    <div className="card-image" data-cy="MovieCardImageContainer">
+      <figure className="image is-4by3">
+        <img
+          data-cy="MovieImage"
+          src={movie.imgUrl}
+          alt={`${movie.title} poster`}
+        />
+      </figure>
+    </div>
+
+    <div className="card-content" data-cy="MovieCardContent">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="IMDb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8" data-cy="MovieTitle">
+            {movie.title}
+          </p>
+        </div>
+      </div>
+
+      <div className="content">
+        <p data-cy="MovieDescription">
+          {movie.description}
+        </p>
+
+        <a
+          href={movie.imdbUrl}
+          data-cy="MovieLink"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          IMDb
+        </a>
+      </div>
+    </div>
+  </div>
+);
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,16 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) => {
+  if (movies.length === 0) {
+    return <div className="movies">No movies to display</div>;
+  }
+
+  return (
+    <div className="movies">
+      {movies.slice(0, 5).map(movie => (
+        <MovieCard movie={movie} key={movie.imdbId} />
+      ))}
+    </div>
+  );
+};
